72af646388cca90f89d06443fc14e15bc725f2fe
wagner rodrigo da silva <wagnerrodrigo@gmail.com>
2018-08-20 16:59:21 -0300

    add novas infor

--- prova-laravel/codigos.sql
+++ prova-laravel/codigos.sql
@@ -1,17 +1,24 @@
-﻿CREATE TABLE Estados(
-CODIGO VARCHAR (2) PRIMARY KEY NOT NULL,
+﻿CREATE TABLE tb_estados(
+sigla INTEGER PRIMARY KEY NOT NULL,
 nome VARCHAR(90) 
 );
 
-CREATE TABLE Cidade(
+CREATE TABLE tb_cidade(
 codigo INTEGER PRIMARY KEY NOT NULL,
 nome VARCHAR(90)
 );
 
-CREATE TABLE Cliente (
+
+CREATE TABLE tb_cliente (
   codigo INTEGER PRIMARY KEY NOT NULL,
   nome VARCHAR(90),
-  cnpj INTERGER(14),
-  telefone_contato INTERGER(12),
+  cnpj VARCHAR(14),
+  telefone_contato VARCHAR(12),
   email_contato VARCHAR(30)
 );
+
+
+ALTER TABLE tb_cidade
+ADD CONSTRAINT FK_id_estado FOREIGN KEY (sigla) REFERENCES tb_estado(silga);
+
+
